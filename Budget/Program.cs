7d46273b63a89ea59db9d7f5a9fcaef3946ae4bb
Sergey Aseev <aseev@devexperts.com>
2026-07-03 21:44:57 +0300

    Keep main page only

--- Budget/Program.cs
+++ Budget/Program.cs
@@ -23,12 +23,9 @@ Console.WriteLine($"{settings.Id} BankSession: id={settings.BankSession.Id}, acc
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
